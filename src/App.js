36100dafe317a7495e635e3baebda96f071ff4d6
jito <jitokj3@gmail.com>
2020-10-10 16:24:17 +0530

    components and containers
    structuring into components,HOC,containers...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
+import Layout from "./components/Layout/layout";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
   }
 }
 
---
+++ src/components/Layout/layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Auxillary from "../../hoc/Auxillary";
+
+const layout = (props) => (
+  <Auxillary>
+    <div></div>
+    <main>{props.children}</main>
+  </Auxillary>
+);
+
+export default layout;
